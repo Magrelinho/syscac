cb0c9e4d546b7ecc3ffca88126aade6591d35d09
Magrelinho <jefferson.ramos88@gmail.com>
2019-10-17 23:36:01 -0300

    ok

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PoMenuItem } from '@portinari/portinari-ui';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  menuItemSelected: string;
+
+  menus: Array<PoMenuItem> = [
+    { label: 'Atendimento', action: this.printMenuAction, icon: 'po-icon-user', shortLabel: 'Atendimento' },
+    { label: 'Timekeeping', action: this.printMenuAction, icon: 'po-icon-clock', shortLabel: 'Timekeeping', badge: { value: 1 } },
+    { label: 'Useful links', icon: 'po-icon-share', shortLabel: 'Links', subItems: [
+      { label: 'Ministry of Labour', action: this.printMenuAction, link: 'http://trabalho.gov.br/' },
+      { label: 'SindPD Syndicate', action: this.printMenuAction, link: 'http://www.sindpd.com.br/' }
+    ]}];
+
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  printMenuAction(menu: PoMenuItem) {
+    this.menuItemSelected = menu.label;
   }
 
 }
