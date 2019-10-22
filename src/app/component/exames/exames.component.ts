73f25928d40e7153197da1821ac95fb40851eaf2
Magrelinho <jefferson.ramos88@gmail.com>
2019-10-22 14:20:45 -0300

    ok nova lista

--- src/app/component/exames/exames.component.ts
+++ src/app/component/exames/exames.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ExamesService } from 'src/app/service/exames.service';
+import { PoTableColumn, PoMultiselectOption, PoCheckboxGroupOption } from '@portinari/portinari-ui';
+import { PoPageAction, PoPageFilter } from '@portinari/portinari-ui';
+import { PoModalAction, PoModalComponent } from '@portinari/portinari-ui';
+import { PoBreadcrumb } from '@portinari/portinari-ui';
+import { PoDialogService } from '@portinari/portinari-ui';
+import { PoNotificationService } from '@portinari/portinari-ui';
 
 @Component({
   selector: 'app-exames',
@@ -7,16 +13,55 @@ import { ExamesService } from 'src/app/service/exames.service';
   styleUrls: ['./exames.component.css']
 })
 export class ExamesComponent implements OnInit {
-  items: [];
+  items: Array<object>;
+  hiringProcesses: Array<any>;
+  status: Array<any>;
+  disclaimerGroup;
+  hiringProcessesColumns: Array<PoTableColumn>;
+  hiringProcessesFiltered: Array<object>;
+  jobDescription: Array<string> = [];
+  jobDescriptionOptions: Array<PoMultiselectOption>;
+  labelFilter = '';
+
+  statusOptions: Array<PoCheckboxGroupOption>;
+
+  public readonly actions: Array<PoPageAction> = [
+    { label: 'Teste'},
+    { label: 'Aprovar'},
+    { label: 'qualquer coisa'},
+    { label: 'Seila'},
+  ];
 
   constructor(private exameService: ExamesService) { }
 
   ngOnInit() {
     this.exameService.listaExamess().subscribe(value => {
-      this.items = value['exames'];
+      // tslint:disable-next-line: no-string-literal
+      this.hiringProcessesColumns = this.getColumns();
+      this.hiringProcesses = value['exames']; 
 
-    })
-    
+      // tslint:disable-next-line: no-shadowed-variable
+      this.hiringProcesses .forEach(value => {
+        value['status_value'] = value.status.descricao;
+
+      });
+    });
+
   }
+
+  
+  getColumns(): Array<PoTableColumn> {
+    return [
+      {
+        property: 'hireStatus', label: 'Status', type: 'subtitle', subtitles: [
+          { value: 'Aprovado', color: 'success', label: 'Aprovado', content: '1' },
+        ]
+      },
+      { property: 'nome', label: 'Nome', type: 'string' },
+      { property: 'tipo_exame', label: 'Tipo Exame', type: 'string' },
+      { property: 'status_value', label: 'Descrição', type: 'string' },
+    ];
+  }
+
 
 }
